50eec7dab503dc034f0a244e3b03dcfc9ffe2db3
Osama Jaber <jbr.osama@gmail.com>
2017-04-22 00:01:20 +0300

    working version

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from ConfigParser import SafeConfigParser
 from resize import resize_handler
-import cStringIO
+import StringIO
 import logging
 import urllib
 import sys
 import json
-from PIL import Image
-import PIL
 import boto
 
 # logger
@@ -24,35 +22,50 @@ sizes=config.get('resizes')
 # resize handler 
 resize = resize_handler()
 
+# image types
+content_types = {
+    'png' : 'image/png',
+    'jpg' : 'image/jpeg',
+    'jpeg' : 'image/jpeg',
+    'bmp'   : 'image/bmp',
+    'gif'   : 'image/gif'
+}
+
 def lambda_handler(event,context):
     for record in event['Records']:
         event_bucket = record['s3']['bucket']['name']
-        if event_bucket == bucket_name : continue
         key = urllib.unquote_plus(record['s3']['object']['key'])
+        image_type = content_types[record['s3']['object']['key'].split('.')[-1].lower()]
         if '/' in key : 
             image_name = key.split('/')[-1]
         else:
             image_name = key
-        try:
-            # get image from s3
-            s3 = boto.connect_s3()
-            bucket = s3.get_bucket(event_bucket, validate=False)
-            k = bucket.new_key(key)
-            image_content = k.get_contents_as_string()
 
-            # do the resize 
-            sizes = config.get('resizes') 
-            for item in sizes:
-                size=item['size']
-                s3_directory=item['directory']
-                resized_image= resize.resize(size=size,image_content=image_content)
-                # Upload the new sizes to s3 again
-                s3 = boto.connect_s3()
-                bucket = s3.get_bucket(bucket_name)
-                k = bucket.new_key('%s/%s'%(s3_directory,image_name))
-                k.set_contents_from_string(resized_image.getvalue())
+        # get image from s3
+        s3 = boto.connect_s3()
+        bucket = s3.get_bucket(event_bucket, validate=False)
+        k = bucket.new_key(key)
+
+        # Save image into FileObject
+        image_file = StringIO.StringIO()
+        k.get_file(image_file)
+
+        # do the resize 
+        sizes = config.get('sizes') 
+        for item in sizes:
+          size=item['size']
+          s3_directory=item['directory']
+          resized_image= resize.resize(size=size,image_content=image_file)
+          # Upload the new sizes to s3 again
+          s3 = boto.connect_s3()
+          bucket = s3.get_bucket(bucket_name)
+          new_k = bucket.new_key('{folder}/{fname}'.format(folder=s3_directory,fname=image_name))
+          new_k.set_acl("public-read")
+          new_k.set_metadata('Content-Type',image_type)
+          new_k.set_contents_from_string(resized_image.getvalue())
+          
         
-        except Exception as e:
-            logger.error(e)
-            logger.error('Error getting object %s from bucket %s.', key, bucket)
+        resized_image.close()
+        image_file.close()
+
         
--- resize.py
+++ resize.py
@@ -1,23 +1,21 @@
 from PIL import Image
-import cStringIO
-import PIL
+import StringIO
 
 class resize_handler():
     def resize(self,size,image_content):
         width=int(size.split('x')[0])
         height=int(size.split('x')[1])
-
         img = Image.open(image_content)
         if width and height:
-            img = img.resize((width, height), PIL.Image.ANTIALIAS)
+            img = img.resize((width, height), Image.ANTIALIAS)
         elif not height:
             wpercent = (width / float(img.size[0]))
             hsize = int((float(img.size[1]) * float(wpercent)))
-            img = img.resize((width, hsize), PIL.Image.ANTIALIAS)
+            img = img.resize((width, hsize), Image.ANTIALIAS)
         elif not width:
             hpercent = (height / float(img.size[1]))
             wsize = int((float(img.size[0]) * float(hpercent)))
-            img = img.resize((wsize, height), PIL.Image.ANTIALIAS)
-        tmp_image = cStringIO.StringIO()
-        img.save(tmp_image)
-        return tmp_image
+            img = img.resize((wsize, height), Image.ANTIALIAS)
+        tmp_img = StringIO.StringIO()
+        img.save(tmp_img,'png')
+        return tmp_img
