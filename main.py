a34fd81f3c0ea20151d0774aea3bf5daa044c2ce
Osama Jaber <jbr.osama@gmail.com>
2017-06-07 11:22:50 +0300

    two s3-buckets, one for originals and another for resized. Also add make_public and content_type:image/jpeg

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ logger = logging.getLogger(__name__)
 
 # config
 config = json.loads(open('config.json').read())
-
-bucket_name=config.get('bucket')
-original_dir=config.get('original').get('directory')
-sizes=config.get('resizes')  
+originals_bucket_name=config.get('originals_bucket')
+resized_bucket_name=config.get('resized_bucket')
 
 # resize handler 
 resize = resize_handler()
@@ -26,9 +24,9 @@ resize = resize_handler()
 content_types = {
     'png' : 'image/png',
     'jpg' : 'image/jpeg',
-    'jpeg' : 'image/jpeg',
-    'bmp'   : 'image/bmp',
-    'gif'   : 'image/gif'
+    'jpeg': 'image/jpeg',
+    'bmp' : 'image/bmp',
+    'gif' : 'image/gif'
 }
 
 def lambda_handler(event,context):
@@ -36,36 +34,35 @@ def lambda_handler(event,context):
         event_bucket = record['s3']['bucket']['name']
         key = urllib.unquote_plus(record['s3']['object']['key'])
         image_type = content_types[record['s3']['object']['key'].split('.')[-1].lower()]
-        if '/' in key : 
-            image_name = key.split('/')[-1]
-        else:
-            image_name = key
-
+        logger.info("Key = %r",key)
+        image_name = key
         # get image from s3
         s3 = boto.connect_s3()
         bucket = s3.get_bucket(event_bucket, validate=False)
-        k = bucket.new_key(key)
-
+        tmp_key = bucket.new_key(key)
         # Save image into FileObject
         image_file = StringIO.StringIO()
-        k.get_file(image_file)
+        tmp_key.get_file(image_file)
 
         # do the resize 
         sizes = config.get('sizes') 
         for item in sizes:
           size=item['size']
+          logger.info("Resizing to %r", size)
           s3_directory=item['directory']
           resized_image= resize.resize(size=size,image_content=image_file)
           # Upload the new sizes to s3 again
-          s3 = boto.connect_s3()
-          bucket = s3.get_bucket(bucket_name)
+          bucket = s3.get_bucket(resized_bucket_name)
+          logger.info("Connected to resized bucket %r", resized_bucket_name)
           new_k = bucket.new_key('{folder}/{fname}'.format(folder=s3_directory,fname=image_name))
-          new_k.set_acl("public-read")
-          new_k.set_metadata('Content-Type',image_type)
+          logger.info("New Key should be in %r",'{folder}/{fname}'.format(folder=s3_directory,fname=image_name))
           new_k.set_contents_from_string(resized_image.getvalue())
-          
+          new_k.make_public()
+          new_k.set_remote_metadata({'Content-Type': image_type},{},True)
+          logger.info("Size=%r should be uploaded",size)
+          resized_image.close()
         
-        resized_image.close()
+        # clost the fileObject
         image_file.close()
 
         
